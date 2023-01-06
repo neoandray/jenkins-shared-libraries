5a8ddec8d74c4a91372609f059163cfbb26b9e37
neoandray <neoandray@gmail.com>
2023-01-06 11:49:55 -0400

    update runPowerCliCommand for cleaner JSON filtering

--- vmware/vars/runPowerCliCommand.groovy
+++ vmware/vars/runPowerCliCommand.groovy
@@ -3,18 +3,18 @@ def call(String environment, String command){
     
         def response = null
         if (environment.toString().toLowerCase() == "test"){
+
+         response =  pwsh( script:"""connect-viserver -server ${vCenterServer7} -user ${vCenterCred7_USR} -password ${vCenterCred7_PSW};  ${command}""", encoding: 'UTF-8',returnStdout:true)  
         
-            response =  pwsh( script:"""connect-viserver -server ${vCenterServer7} -user ${vCenterCred7_USR} -password ${vCenterCred7_PSW};  ${command}""", encoding: 'UTF-8',returnStdout:true)  
         } else{
-         
-            response =  pwsh( script:"""connect-viserver -server ${vCenterServer6} -user ${vCenterCred6_USR} -password ${vCenterCred6_PSW};  ${command}""", encoding: 'UTF-8',returnStdout:true)  
+
+                response =  pwsh( script:"""connect-viserver -server ${vCenterServer6} -user ${vCenterCred6_USR} -password ${vCenterCred6_PSW};  ${command}""", encoding: 'UTF-8',returnStdout:true)  
         }
-        
-        def processedResponse= response.replace("""Name                           Port  User
-----                           ----  ----
-172.22.38.56                   443   VSPHERE.LOCAL\\gsp_devops""",'').trim()
-        processedResponse= processedResponse.replace("""Name                           Port  User
-----                           ----  ----
-172.22.38.50                   443   VSPHERE.LOCALCM\\gsp_devops""",'').trim()
+        def processedResponse=  null
+        def jsonStart  = response.indexOf('{')
+        if (jsonStart){ 
+         processedResponse = response.substring(jsonStart) 
+
+        }
         return processedResponse
 }
